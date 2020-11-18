12fd881da9ee93b033b038c51740dba011127458
Jocelyn <joeads@iu.edu>
2020-11-17 21:34:31 -0500

    giving up for today

---
+++ Bee/Assets/Scripts/LevelScripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Collider flowerCollider;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        flowerCollider = collision.collider;
+        if (Input.GetKeyDown("space"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
